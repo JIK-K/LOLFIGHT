774a1081505a30b14da521399d8eb1e93fc045a1
abcdaramgi <wwa1102@naver.com>
2024-03-04 16:34:09 +0900

    [feat] - 에디터 이미지 등록 저장

---
+++ src/api/post.api.ts
@@ -0,0 +1,38 @@
+import constant from "../common/constant/constant";
+import axios, { AxiosResponse } from "axios";
+import { PostDTO } from "../common/DTOs/board/post.dto";
+import { ResponseDTO } from "../common/DTOs/response.dto";
+
+const baseUrl = `${constant.SERVER_URL}/post`;
+
+/**
+ * 게시글 작성
+ * @param postDTO
+ * @returns
+ */
+export const writePost = async (
+  postTitle: string,
+  postContent: string,
+  postWriter: string,
+  postBoard: string
+): Promise<AxiosResponse<ResponseDTO<PostDTO>>> => {
+  let url = `${baseUrl}`;
+
+  const formData = new FormData();
+  formData.append("postTitle", postTitle);
+  formData.append("postContent", postContent);
+  formData.append("postWriter", postWriter);
+  formData.append("postBoard", postBoard);
+
+  console.log("FormData contents:");
+  formData.forEach((value: FormDataEntryValue, key: string) => {
+    console.log(key + ", " + value);
+  });
+  return await axios.post(url, formData);
+};
+
+/**
+ * 게시글 목록 조회
+ * @param postDTO
+ * @returns
+ */
--- src/common/components/WysiwygEditor.tsx
+++ src/common/components/WysiwygEditor.tsx
@@ -5,8 +5,28 @@ import "@toast-ui/editor/dist/toastui-editor.css";
 import "@toast-ui/editor/dist/theme/toastui-editor-dark.css";
 // import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css";
 import colorSyntax from "@toast-ui/editor-plugin-color-syntax";
+<<<<<<< Updated upstream
 
 const WysiwygEditor = () => {
+=======
+import boardNavLinks from "@/src/data/boardNavLinks";
+import { useEffect, useRef } from "react";
+import { writePost } from "@/src/api/post.api";
+import constant from "@/src/common/constant/constant";
+import { useState } from "react";
+import { PostDTO } from "@/src/common/DTOs/board/post.dto";
+import axios from "axios";
+import { blob } from "stream/consumers";
+import { on } from "events";
+import { useRouter } from "next/navigation";
+
+const WysiwygEditor = () => {
+  const router = useRouter();
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [image, setImage] = useState("");
+  const editorRef = useRef<Editor>(null);
+>>>>>>> Stashed changes
   const toolbarItems = [
     ["heading", "bold", "italic", "strike"],
     ["hr"],
@@ -17,6 +37,77 @@ const WysiwygEditor = () => {
     ["scrollSync"],
   ];
 
+<<<<<<< Updated upstream
+=======
+  useEffect(() => {
+    if (editorRef.current) {
+      editorRef.current.getInstance().removeHook("addImageBlobHook");
+
+      editorRef.current
+        .getInstance()
+        .addHook("addImageBlobHook", (blob: any, callback: any) => {
+          onUploadImage(blob, callback);
+        });
+    }
+  }, [editorRef]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+    console.log(title);
+  };
+
+  const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCategory(e.target.value);
+    console.log(category);
+  };
+
+  const handleCancleClick = () => {
+    console.log("취소");
+    console.log(constant.SERVER_URL);
+  };
+
+  const handleSaveClick = async () => {
+    const link = "";
+    const editorIns = editorRef.current?.getInstance().getHTML() || "";
+    writePost(title, editorIns, "작성자", category).then((response) => {
+      console.log(response);
+      boardNavLinks
+        .filter((link) => link.href !== "/")
+        .map((link) => {
+          if (link.title === category) {
+            router.replace(link.href + "/" + response.data.data.id);
+          }
+        });
+      return;
+    });
+  };
+
+  const onUploadImage = async (blob: any, callback: any) => {
+    console.log(blob);
+    const formData = new FormData();
+    formData.append("file", blob);
+    try {
+      const imageRes = await axios.post(
+        `${constant.SERVER_URL}/post/image`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      console.log("imageRes", imageRes);
+      console.log("imageRes", imageRes.data.data);
+      const imageUrl = `${constant.SERVER_URL}/` + imageRes.data.data;
+      console.log("imgurl", imageUrl);
+      setImage(imageUrl);
+      callback(imageUrl, "image");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+>>>>>>> Stashed changes
   return (
     <div className="flex flex-col justify-center items-center h-full">
       <input
