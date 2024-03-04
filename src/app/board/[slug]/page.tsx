a15b5cf588a0aaf3d32da443624fbcfb8ee393af
abcdaramgi <wwa1102@naver.com>
2024-03-05 00:36:43 +0900

    [feat] - get board list

--- src/app/board/[slug]/page.tsx
+++ src/app/board/[slug]/page.tsx
@@ -8,21 +8,16 @@ type PageProps = {
 };
 
 export default function Page({ params }: { params: PageProps }) {
-  const title = getTitleFromSlug(params.slug);
+  // const title = getTitleFromSlug(params.slug);
 
   return (
     <>
       <div className="w-full h-full mt-16">
         <div className="w-1200px h-full mx-auto flex">
           <BoardNavComponent></BoardNavComponent>
-          <BoardComponent title={title}></BoardComponent>
+          <BoardComponent slug={params.slug}></BoardComponent>
         </div>
       </div>
     </>
   );
-}
-
-function getTitleFromSlug(slug: string) {
-  const link = boardNavLinks.find((link) => link.href === `/board/${slug}`);
-  return link?.title ?? "";
 }
--- src/data/boardNavLinks.ts
+++ src/data/boardNavLinks.ts
@@ -1,7 +1,13 @@
 const boardNavLinks = [
+<<<<<<< Updated upstream
   { href: "/board/all", title: "전체 게시판" },
   { href: "/board/free", title: "자유 게시판" },
   { href: "/board/fight", title: "싸움 게시팔" },
+=======
+  { href: "/board/all", title: "전체" },
+  { href: "/board/free", title: "자유" },
+  { href: "/board/fight", title: "싸움" },
+>>>>>>> Stashed changes
 ];
 
 export default boardNavLinks;
