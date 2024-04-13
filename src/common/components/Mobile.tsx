6408384e62793414281091000cc63f25aaf1ff7b
JIK-K <dnswlrsla@gmail.com>
2024-04-13 12:55:00 +0900

    feat: mobile page

---
+++ src/common/components/Mobile.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Mobile = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-200">
+      <div className="bg-white rounded-lg shadow-lg p-8">
+        <h1 className="text-3xl font-bold mb-4">LOLFIGHT</h1>
+        <p className="text-lg text-gray-600">
+          LOLFIGHT does not support mobile{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Mobile;
