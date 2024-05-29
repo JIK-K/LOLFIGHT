5803ec0c5e716b1313ca1639badb815990484249
JIK-K <dnswlrsla@gmail.com>
2024-05-29 16:09:13 +0900

    feat: borad pagination

--- src/app/board/components/BoardComponent.tsx
+++ src/app/board/components/BoardComponent.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import BoardInfoComponent from "./BoardInfoComponent";
-import BoardHeadComponent from "./BoardHeadComponent";
-import { PostDTO } from "@/src/common/DTOs/board/post.dto";
 import { useEffect, useState } from "react";
 import { getPostList } from "@/src/api/post.api";
 import boardNavLinks from "@/src/data/boardNavLinks";
+import Pagination from "@mui/material/Pagination";
+import BoardInfoComponent from "./BoardInfoComponent";
+import BoardHeadComponent from "./BoardHeadComponent";
+import { PostDTO } from "@/src/common/DTOs/board/post.dto";
 
 interface BoardComponentProps {
   slug: string;
@@ -18,13 +19,29 @@ function getTitleFromSlug(slug: string) {
 
 const BoardComponent = (props: BoardComponentProps) => {
   const [postList, setPostList] = useState<PostDTO[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0); // 총 페이지 수
+  const postsPerPage = 15;
 
   useEffect(() => {
     getPostList(`${getTitleFromSlug(props.slug)}`).then((res) => {
       console.log(res);
       setPostList(res.data.data);
+      setTotalPages(Math.ceil(res.data.data.length / postsPerPage));
     });
   }, []);
+
+  const handlePageClick = (
+    event: React.ChangeEvent<unknown>,
+    pageNumber: number
+  ) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const paginatedPosts = postList.slice(
+    (currentPage - 1) * postsPerPage,
+    currentPage * postsPerPage
+  );
 
   return (
     <div className="notice w-full bg-white ml-8 shadow-md">
@@ -36,13 +53,21 @@ const BoardComponent = (props: BoardComponentProps) => {
         ></BoardHeadComponent>
       </div>
       <div className="notice__content">
-        {postList.map((post) => (
+        {paginatedPosts.map((post) => (
           <BoardInfoComponent
             key={post.id}
             data={post}
             slug={props.slug}
           ></BoardInfoComponent>
         ))}
+      </div>
+      <div className="notice__pagination w-full flex justify-center mt-3 p-3 border-t-2 border-brandcolor">
+        <Pagination
+          count={totalPages}
+          shape="rounded"
+          boundaryCount={2}
+          onChange={(event, page) => handlePageClick(event, page)}
+        />
       </div>
     </div>
   );
